3d7330450ecd9dd6b4fcc04ae4d8bf7f1b65555a
Nelisiwe Mkhabela <nelisiwecynthia39@gmail.com>
2022-09-08 16:39:33 -0700

    cPuts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,5 +3,7 @@
  * Entry point
  * Return (0)
  */
-int main(void){puts("\"Programming is like building a multilingual puzzle");
-	     return (0);}
+int main(void){
+	puts("\"Programming is like building a multilingual puzzle");
+	     return (0);
+}
