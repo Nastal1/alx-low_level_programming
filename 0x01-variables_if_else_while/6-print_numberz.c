e15758546739f03c288ef189fb73579990dab9fe
Nelisiwe Mkhabela <nelisiwecynthia39@gmail.com>
2022-09-12 07:19:25 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdli.h>
+#include <time.h>
+#include <stduio>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int alpha;
+
+	alpha = 78;
+	while (alpha < 88)
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	putchar('\n');
+	return (0);
+}
