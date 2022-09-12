d93efc133f8335f1d33a436b38fc6c80ba365297
Nelisiwe Mkhabela <nelisiwecynthia39@gmail.com>
2022-09-12 07:38:46 -0700

    tebalpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'tabalpha prints'
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char smile;
+
+	for (smile = 'z'; smile >= 'a'; smile--)
+	{
+		putchar(smile);
+	}
+	putchar(\n');
+	return (0);
+}
