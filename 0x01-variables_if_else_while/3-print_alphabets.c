08421d1a40cd6f59160defc4eb432c777d5e124e
Nelisiwe Mkhabela <nelisiwecynthia39@gmail.com>
2022-09-12 05:27:15 -0700

    aA

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/*
+ * main - entry point
+ * Description: 'Print alphabets in lower and upper case'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alpha;
+
+	alpha = 'a';
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+		alpha++
+	}
+	alpha = 'A';
+	while (alpha <= 'Z')
+	{
+		putchar(alpha);
+		alpha++
+	}
+	putchar('\n');
+	return (0);
+}
