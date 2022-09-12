ae0bdddb03fcab0b248489f8530cb9e3ca6d1f56
Nelisiwe Mkhabela <nelisiwecynthia39@gmail.com>
2022-09-12 08:00:37 -0700

    print combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int y;
+
+	for (y = 48; y < 58; y++)
+	{
+		putchar(y);
+
+		if (y != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+}
