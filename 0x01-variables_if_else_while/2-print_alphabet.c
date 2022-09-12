0490add451432f287f65b8595c7c85cf926d66d9
Nelisiwe Mkhabela <nelisiwecynthia39@gmail.com>
2022-09-12 04:55:35 -0700

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*
+ * main entry point
+ * Description:'prints alphabeths in lower case'
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char 1;
+	1 = 'a';
+	while (1<='z')
+	{
+		putchar(1);
+		1++
+	}
+	putchar('\n');
+	return (0);
+}
