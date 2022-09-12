b2bdacbe126b1e9a560b015ce7d51b08f328d817
Nelisiwe Mkhabela <nelisiwecynthia39@gmail.com>
2022-09-12 04:16:22 -0700

    1 last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,7 +25,7 @@ int main(void)
 	}
 	else
 	{
-	printf("Last digit of %d is %d and is lwss than 6 and not zero\n", n, r);
+	printf("Last digit of %d is %d and is less than 6 and not zero\n", n, r);
 	}
 	return (0);
 }
