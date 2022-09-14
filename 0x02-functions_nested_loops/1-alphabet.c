50132a3584b32cb0c94e5f7da7661b6a07a6d4bc
Nelisiwe Mkhabela <nelisiwecynthia39@gmail.com>
2022-09-14 01:44:24 -0700

    alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * Print_alphabeth - function that prints a-z in lowercase followed by a new line'
+ * print_alphabeth - Entry
+ * Description: 'prints a-z in lowercase followed by a new line'
  * Return: Nothing
 */
 void print_alphabet(void)
