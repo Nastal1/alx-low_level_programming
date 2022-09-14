479ea706ecfe4e2ba24c8952d659f3e2f9756696
Nelisiwe Mkhabela <nelisiwecynthia39@gmail.com>
2022-09-13 22:35:48 -0700

    prints a to z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.c"
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'prints a-z in lowercase followed by a new line'
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha < 'z')
+	{
+		_putchar(alpha);
+		alpha++
+	}
+	_putchar('\n');
+	return (0);
+}
