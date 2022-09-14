d9f2e2ceb042a7b6de5b80e1e98c62940623bcf9
Nelisiwe Mkhabela <nelisiwecynthia39@gmail.com>
2022-09-14 05:55:07 -0700

    adds

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,5 +9,5 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-void print_to_98(int alpha);
-void prit_times_table(int alpha);
+void print_to_98(int n);
+void prit_times_table(int n);
