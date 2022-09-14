079b5ddbd5d206b2f3ea9643c8a57d5e591cadc0
Nelisiwe Mkhabela <nelisiwecynthia39@gmail.com>
2022-09-13 23:49:46 -0700

    x10 print alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,18 @@
- 
 #include "main.h"
-/** 
- * print_alphabet_x10 - repeats the print_alphabet 10 times. 
- * Return: Nothing. 
-*/ 
-void print_alphabet_x10(void) 
+
+/**
+ * Print_alphabet_x10 - Entry point
+ * Description: 'the program prints all alphabrts x10'
+ * Return: Nothing
+*/
+void Print_alphabet_x10(void)
 {
-	int i;
-	char c;
-	
-	for (i = 0; i < 10; ++i)
+	int i;
+	char c;
+
+	for (i = 0; i < 10; i++)
 	{
-		for (c = 'a'; c <= 'z'; c++)
+		for (c = 'a'; c < 'z'; c++)
 		{
 			_putchar(c);
 		}
