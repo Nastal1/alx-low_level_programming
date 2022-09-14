1717303a3127cfed16796dabc404a1bafc9eefea
Nelisiwe Mkhabela <nelisiwecynthia39@gmail.com>
2022-09-14 02:16:46 -0700

    islower

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 int _putchar(int alpha);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _islower(int alpha);
+int _islower(int e);
 int _isalpha(int alpha);
 int print_sign(int alpha);
 int _abs(int);
