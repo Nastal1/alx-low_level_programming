3ced68d01293dc049a9a5a794c9bfa897e1e9d3f
Nelisiwe Mkhabela <nelisiwecynthia39@gmail.com>
2022-09-14 03:07:29 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,8 +3,8 @@
 /**
  * _isalpha - prints one or zero depending on input
  * @c: collects a char type
- * Description: Checks for alphabets lower and uppercase
- * Return: Always(0)
+ * Description: Chrcks for alphabets lower and uppercase
+ * Return: Always (0)
 */
 int _isalpha(int c)
 {
