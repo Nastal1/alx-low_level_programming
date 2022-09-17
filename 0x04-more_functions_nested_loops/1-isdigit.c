edd54f3ba48f7ee314af825a87124fa7a8bcc6d7
Nelisiwe Mkhabela <nelisiwecynthia39@gmail.com>
2022-09-16 18:52:45 -0700

    is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+/**
+ * _isdigit - checks to see it is a dogit or alphabeth
+ * Description: prints digit using _putchar
+ * @c: character
+ * Return: void
+*/
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
