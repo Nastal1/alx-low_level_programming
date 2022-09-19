7c8acb22a2b63c57f6cfc950fa22e2d5722c2d32
Nelisiwe Mkhabela <nelisiwecynthia39@gmail.com>
2022-09-19 13:26:48 -0700

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,25 +6,15 @@
  */
 void rev_string(char *s)
 {
-	int a, len;
+	int i = 0;
+	int j = (_strlen(s) - 1);
+	char rdc;
 
-	char *begin, *end = s;
-
-	for (a = 0; s[a] != '\0' && s[a + 1] != '\0' ; a++)
+	while (i < j)
 	{
-		end++;
+		rdc = s[i];
+		s[i] = s[j];
+		s[j] = rdc;
+		i++, j--;
 	}
-	len = a + 1;
-	begin = s;
-	for (a = 0; a < len / 2; a++)
-	{
-		char x;
-
-		x = *end;
-		*end = *begin;
-		*begin = x;
-		begin++;
-		end--;
-	}
-	end[len + 1] = '\0';
 }
