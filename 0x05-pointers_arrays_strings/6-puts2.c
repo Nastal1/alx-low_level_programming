e3b295c6baa310319721b922738b62dac9bcb8c0
Nelisiwe Mkhabela <nelisiwecynthia39@gmail.com>
2022-09-19 06:30:31 -0700

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * puts2 - function that prints every other character of a string, 
- * starting with the first character, 
+ * puts2 - function that prints every other character of a string,
+ * starting with the first character,
  * followed by a new line
  * @s: pointer points to a character or string
  */
