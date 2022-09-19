a8cf8fd668389fad06eef16fbec28f9acff9806e
Nelisiwe Mkhabela <nelisiwecynthia39@gmail.com>
2022-09-19 05:13:30 -0700

    print rev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rev_string - function prints a string reverser
+ * @s: pointer pointing to a string
+ */
+void rev_string(char *s)
+{
+	int a, len;
+
+	char *begin, *end = s;
+
+	for (a = 0; s[a] != '\0' ; a++)
+	{
+		end++;
+	}
+	len = a + 1;
+	begin = s;
+	for (a = 0; a < len / 2; a++)
+	{
+		char x;
+
+		x = *end;
+		*end = *begin;
+		*begin = x;
+		begin++;
+		end--;
+	}
+	end[len + 1] = '\0';
+}
