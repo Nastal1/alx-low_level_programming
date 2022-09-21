3990436f3cdc85f0da8561f13881ee438e98e6d0
Nelisiwe Mkhabela <nelisiwecynthia39@gmail.com>
2022-09-21 09:20:37 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,7 +11,7 @@ char *_strcat(char *dest, char *src)
 
 	for (i = 0; dest[i] != '\0'; i++)
 		;
-	for (j = 0; src[j] != '\n'; j++)
+	for (j = 0; src[j] != '\0'; j++)
 	{
 		dest[i] = src[j];
 		i++;
