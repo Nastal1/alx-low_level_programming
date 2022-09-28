69feddaadbf608e4af43fab2999adbe4f1dd3c23
Nelisiwe Mkhabela <nelisiwecynthia39@gmail.com>
2022-09-28 07:38:54 -0700

    wildcmp

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * wildcmp - compare strings
+ * @s1: pointer to string param
+ * @s2: pointer to string param
+ * Return: 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 != '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+	}
+}
