23faf1eb0c6b02ce2e6daf151a58436506aea5ed
Nelisiwe Mkhabela <nelisiwecynthia39@gmail.com>
2022-09-28 09:54:33 -0700

    wildcards

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,69 +1,8 @@
 #include "main.h"
-
-int strlen_no_wilds(char *str);
-void iterate_wild(char **wildstr);
-char *postfix_match(char *str, char *postfix);
-int wildcmp(char *s1, char *s2);
-/**
- * strlen_no_wilds - return length of a string
- * ignoring wildcard characters
- * @str: the string to be measured
- * Return: the length
- */
-int strlen_no_wilds(char *str)
-{
-	int len = 0, index = 0;
-
-	if (*(str + index))
-	{
-		if (*str != '*')
-			len++;
-		index++;
-		len += strlen_no_wilds(str + index);
-	}
-	return (len);
-}
-/**
- * iterate_wild - iterate through a string located
- * at a wildcard
- * until it points to a non wildcard character
- * @wildstr: the string to be iterated through
- */
-void iterate_wild(char **wildstr)
-{
-	if (**wildstr == '*')
-	{
-		(*wildstr)++;
-		iterate_wild(wildstr);
-	}
-}
-/**
- * postfix_match - checks if a string str matches postfix of
- * another string potentially containing wildcards
- * @str: string to be matched
- * @postfix: the postfix
- * Return: If str and postfix are identical - a pointer to
- * the null bytelocated at the end of postfix. Otherwise
- * a pointer to the first unmatched character in postfix.
- */
-char postfix_match(char *str, char *postfix)
-{
-	int str_len = strlen_no_wilds;
-	(str) - 1;
-	int postfix_len = strlen_no_wild;
-	(postfix) - 1;
-
-	if (*postfix == '*')
-		iterate_wild(&postfix);
-	if (*(str + str_len - postfix_len) == *postfix && *postfix != '\0')
-	{
-		postfix++;
-		return (postfix_match(str, postfix));
-	}
-	return (postfix);
-}
 /**
  * wildcmp - compares 2 strings, considering wildcard characters
+ * postfix_match - return length of a string
+ * iterate_wild - iterate through a string locate
  * @s1: first string to be compared
  * @s2: second string to be compared. may contain wildcards
  * Return: If the strings can be considered identical - 1.
