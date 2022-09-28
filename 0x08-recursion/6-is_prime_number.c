cfa6fbdf4a548da000f7330c7253bba50677385f
Nelisiwe Mkhabela <nelisiwecynthia39@gmail.com>
2022-09-28 06:48:45 -0700

    prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * prime2 - makes possible to evaluate from 1 to n.
+ * @a: same number as n
+ * @b: number iterating from 1 to n.
+ * Return: on Success 1.
+ * on err, -1 is returned and
+ * error no. set appropriately
+ */
+int prime2(int a, int b)
+{
+	if (a == b)
+		return (1);
+	if (a % b == 0)
+		return (0);
+	return (prime2(a, b + 1));
+}
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: integer number
+ * Return: on Success 1.
+ * on err, -1 is returned and
+ * err no. is set appropriately
+ */
+int is_prime_number(int n)
+{
+	if (n <= 0)
+		return (0);
+	return (prime2(n, 2));
+}
