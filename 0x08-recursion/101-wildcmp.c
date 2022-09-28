8d617b1dbbdf1b1f32042f9214f566035e0ef5cf
Nelisiwe Mkhabela <nelisiwecynthia39@gmail.com>
2022-09-28 09:44:41 -0700

    wildcards

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
-int strlen_no_wilds(char *str);
-void iterate_wild(char **wildstr);
-char *postfix_match(char *str, char *postfix);
-int wildcmp(char *s1, char *s2);
+int strlen_no_wilds(char *str);
+void iterate_wild(char **wildstr);
+char *postfix_match(char *str, char *postfix);
+int wildcmp(char *s1, char *s2);
 /**
  * strlen_no_wilds - return length of a string
  * ignoring wildcard characters
